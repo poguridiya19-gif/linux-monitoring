541c75e68571057cba50c110b1e03700463378f5
Poguri Diya <diya>
2026-05-26 16:34:00 +0530

    linux

--- service.sh
+++ service.sh
@@ -2,16 +2,18 @@
 
 SERVICE="NGINX"
 
-echo "=====Service Status====="
+echo "=====Service Status=====" &>>logs/monitor.log
 
 if systemctl is-active --quiet $SERVICE
 
 then
 
-   echo "$SERVICE is running"
+   echo "$SERVICE is running" &>>logs/monitor.log
 
 else
 
-   echo "$SERVICE is NOT running"
+   echo "$SERVICE is NOT running" &>>logs/monitor.log
 
 fi
+
+echo "========================" &>>logs/monitor.log
