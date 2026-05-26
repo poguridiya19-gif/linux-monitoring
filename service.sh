0600dcf6e564d335ca5459e86891766faa3c505e
Poguri Diya <diya>
2026-05-26 16:30:52 +0530

    linux

--- service.sh
+++ service.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 SERVICE="NGINX"
-echo "=====Service Status====="
-if systemctl is-active --quiet $SERVICE"
-then
-   echo "$SERVICE is running"
-else 
-   echo "$SERVICE is NOT running"
-fi
 
+echo "=====Service Status====="
+
+if systemctl is-active --quiet $SERVICE"
+
+then
+
+   echo "$SERVICE is running"
+
+else
+
+   echo "$SERVICE is NOT running"
+   
+fi
