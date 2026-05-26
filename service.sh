ca885fe33ae2dc40a13c8f99f856772b9f22fbd9
Poguri Diya <diya>
2026-05-26 16:23:56 +0530

    linux

--- service.sh
+++ service.sh
@@ -5,6 +5,6 @@ echo "=====Service Status====="
 if systemctl is-active --quiet $SERVICE" ; then
    echo "$SERVICE is running"
 else 
-   echo "$SERVICE is not-running"
+   echo "$SERVICE is NOT running"
 fi
 echo "========================" 
