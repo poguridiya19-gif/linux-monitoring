8524475006bc034539a87839db35687ed61a3fe3
Poguri Diya <diya>
2026-05-26 16:21:35 +0530

    linux

--- disk.sh
+++ disk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "======diskusuage======" &>>logs/monitor.log
-df -H &>>logs/monitor.log
-echo "======================" &>>logs/monitor.log
+echo "======diskusuage======" 
+df -H 
+echo "======================" 
