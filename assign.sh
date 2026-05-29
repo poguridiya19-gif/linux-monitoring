3e78d81afd5f989457f7c4e09c45914312996a99
Poguri Diya <diya>
2026-05-29 17:58:15 +0530

    linux

--- assign.sh
+++ assign.sh
@@ -7,3 +7,14 @@ fi
 
 PATH_NAME=$1 
 
+if [ -e $PATH_NAME ]; then
+   echo "exists: $PATH_NAME"
+
+    if [ -r $PATH_NAME ]; then
+       echo "readable: yes"
+    else
+       echo "readable: no"
+    fi
+
+fi
+    
