5710fada259ce58d0e6b2b75275f0d9bcea967c7
Poguri Diya <diya>
2026-05-29 18:46:00 +0530

    linux

--- logs/count.sh
+++ logs/count.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if [$# -eq 0]; then
+if [ $# -eq 0 ]; then
    echo "usage: $0 test.txt"
    exit 1
 fi
 
 FILE_NAME=$1
 
-if [! -e "$FILE_NAME"]; then
+if [ ! -e "$FILE_NAME"]; then
     echo "error: file does not exist."
     exit 1
 fi
 
-if [! -r "$FILE_NAME"]; then
+if [ ! -r "$FILE_NAME" ]; then
    echo "error: permission denied."
    exit 1
 fi
 
-if [! -s "$FILE_NAME"]; then
+if [ ! -s "$FILE_NAME" ]; then
    echo "file is empty."
    echo "lines: 0"
    echo "words: 0"
