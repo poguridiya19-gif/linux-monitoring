c80c97e8917136ce394c27f2e52f1d4e40f36f31
Poguri Diya <diya>
2026-05-29 18:04:08 +0530

    linux

--- assign.sh
+++ assign.sh
@@ -9,7 +9,7 @@ PATH_NAME=$1
 
 if [ -e "$PATH_NAME" ]; then
 
-   echo "exists: $PATH_NAME"
+    echo "exists: $PATH_NAME"
 
     if [ -r "$PATH_NAME" ]; then
        echo "readable: yes"
@@ -17,5 +17,13 @@ if [ -e "$PATH_NAME" ]; then
        echo "readable: no"
     fi
 
+    if [ -w "$PATH_NAME" ]; then
+       echo "writable: yes"
+    else 
+       echo "writable: no"
+    fi
+
+else
+    echo "file or directory does NOT exist."
 fi
     
